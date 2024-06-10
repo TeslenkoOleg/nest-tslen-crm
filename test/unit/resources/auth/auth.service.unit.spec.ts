d13412147f557f5aeec8503e327b7e13dc4e6c18
Oleh Teslenko <Teslenkooleg2017@gmail.com>
2024-06-10 17:22:20 +0300

    Test running test before commit

--- test/unit/resources/auth/auth.service.unit.spec.ts
+++ test/unit/resources/auth/auth.service.unit.spec.ts
@@ -68,7 +68,6 @@ describe('AuthService signIn', () => {
         if (!isMatchedPassword) {
             error = new UnauthorizedException(); // The line you want to test
         }
-        //
         expect(error).toBeInstanceOf(UnauthorizedException);
         fail('UnauthorizedException not thrown'); // If we reach this point, the test failed
     });
