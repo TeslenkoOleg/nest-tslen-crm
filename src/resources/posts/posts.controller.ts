eff25acd7423d0a7dcedc4f02fe5e82c46ac14d8
Oleh Teslenko <Teslenkooleg2017@gmail.com>
2024-06-29 18:30:56 +0300

    Add role guard

---
+++ src/resources/posts/posts.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { User } from '../users/decorators/user.decorator';
+import { Users } from '../users/entities/users.entity';
+import { Posts } from './entities/post.entity';
+import { DeleteResult } from 'typeorm';
+import { Roles } from '../../common/roles/roles.decorator';
+import { Role } from '../../common/roles/role.enum';
+
+@Controller('posts')
+export class PostsController {
+    constructor (private readonly postsService: PostsService) {}
+
+  @Post()
+  @Roles(Role.Admin)
+    create (@Body() createPostDto: CreatePostDto): Promise<Posts> {
+        return this.postsService.create(createPostDto);
+    }
+
+  @Get()
+  findAll (@User() user: Users): Promise<Posts[]> {
+      return this.postsService.findAll(user);
+  }
+  @Patch(':id')
+  @Roles(Role.Admin)
+  update (@Param('id', ParseIntPipe) id: number, @Body() updatePostDto: UpdatePostDto): Promise<Posts> {
+      return this.postsService.update(+id, updatePostDto);
+  }
+
+  @Delete(':id')
+  @Roles(Role.Admin)
+  remove (@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+      return this.postsService.delete(+id);
+  }
+}
---
+++ src/resources/posts/posts.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Posts } from './entities/post.entity';
+import { PostsRepository } from './posts.repository';
+import { RolesGuard } from '../../common/roles/roles.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Posts]),
+    ],
+    controllers: [PostsController],
+    providers: [
+        PostsService,
+        PostsRepository,
+        JwtService,
+        {
+            provide: APP_GUARD,
+            useClass: RolesGuard,
+        },
+    ],
+})
+export class PostsModule {}
